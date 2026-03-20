097393b63cb5db5a90de207d5264c5dbad9c0e96
Willians96 <146501267+Willians96@users.noreply.github.com>
2026-03-20 20:33:20 -0300

    Fix ML scraper ignoring discounts by mapping sequential fractions and cents

--- netlify/functions/get-price.js
+++ netlify/functions/get-price.js
@@ -41,43 +41,39 @@ exports.handler = async function(event, context) {
          const endIndex = Math.min(formatedHtml.length, firstFractionIndex + 3000);
          const mainProductBlock = formatedHtml.substring(startIndex, endIndex);
 
-         let foundDiscount = false;
-         const matchPrevTag = mainProductBlock.match(/class=["'][^"']*andes-money-amount--previous[^"']*["']/i);
-         
-         if (matchPrevTag) {
-             const prevStart = matchPrevTag.index;
-             const prevBlock = mainProductBlock.substring(prevStart, prevStart + 400);
+         // Mapear todas as frações e seus respectivos centavos
+         const fractionRegex = /<span class="andes-money-amount__fraction"[^>]*>([^<]+)<\/span>/gi;
+         const fractionMatches = Array.from(mainProductBlock.matchAll(fractionRegex));
+         let parsedPrices = [];
+
+         for (const match of fractionMatches) {
+             const fractionVal = match[1];
+             const lookAhead = mainProductBlock.substring(match.index + match[0].length, match.index + match[0].length + 120);
              
-             const matchPrevFra = prevBlock.match(/<span class="andes-money-amount__fraction"[^>]*>([^<]+)<\/span>/i);
-             const matchPrevCen = prevBlock.match(/<span class="andes-money-amount__cents[^>]*>([^<]+)<\/span>/i);
+             const nextFractionIdx = lookAhead.indexOf('andes-money-amount__fraction');
+             const centsIdx = lookAhead.indexOf('andes-money-amount__cents');
              
-             if (matchPrevFra) {
-                 originalPrice = "R$ " + matchPrevFra[1] + "," + (matchPrevCen ? matchPrevCen[1] : "00");
-                 
-                 const endOfPrevFra = prevStart + prevBlock.indexOf(matchPrevFra[0]) + matchPrevFra[0].length;
-                 const remainingHtml = mainProductBlock.substring(endOfPrevFra);
-                 
-                 const matchFracao = remainingHtml.match(/<span class="andes-money-amount__fraction"[^>]*>([^<]+)<\/span>/i);
-                 const matchCentavos = remainingHtml.match(/<span class="andes-money-amount__cents[^>]*>([^<]+)<\/span>/i);
-                 
-                 if (matchFracao) {
-                     price = "R$ " + matchFracao[1] + "," + (matchCentavos ? matchCentavos[1] : "00");
-                     foundDiscount = true;
-                 }
+             let centsVal = "00";
+             if (centsIdx !== -1 && (nextFractionIdx === -1 || centsIdx < nextFractionIdx)) {
+                 const cMatch = lookAhead.match(/<span class="andes-money-amount__cents[^>]*>([^<]+)<\/span>/i);
+                 if (cMatch) centsVal = cMatch[1];
              }
-         }
-         
-         if (!foundDiscount) {
-             const matchFracao = mainProductBlock.match(/<span class="andes-money-amount__fraction"[^>]*>([^<]+)<\/span>/i);
-             const matchCentavos = mainProductBlock.match(/<span class="andes-money-amount__cents[^>]*>([^<]+)<\/span>/i);
-             if (matchFracao) {
-                 price = "R$ " + matchFracao[1] + "," + (matchCentavos ? matchCentavos[1] : "00");
-             }
+             parsedPrices.push({ fraction: fractionVal, cents: centsVal });
          }
 
          const matchDiscount = mainProductBlock.match(/>(\d+%?\s*OFF)</i);
          if (matchDiscount) {
-            discount = matchDiscount[1];
+             if (parsedPrices.length >= 2) {
+                 originalPrice = "R$ " + parsedPrices[0].fraction + "," + parsedPrices[0].cents;
+                 price = "R$ " + parsedPrices[1].fraction + "," + parsedPrices[1].cents;
+                 discount = matchDiscount[1];
+             } else if (parsedPrices.length === 1) {
+                 price = "R$ " + parsedPrices[0].fraction + "," + parsedPrices[0].cents;
+             }
+         } else {
+             if (parsedPrices.length >= 1) {
+                 price = "R$ " + parsedPrices[0].fraction + "," + parsedPrices[0].cents;
+             }
          }
       }
 
